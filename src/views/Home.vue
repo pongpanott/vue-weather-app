92e46b1fc3a785f2fdc9dafe058b1bfea0876625
Pongpanot <fhyptr@gmail.com>
2021-11-11 16:45:50 +0700

    update styles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,37 @@
 <template>
-  <c-stack maxW="sm" mx="auto">
-    <c-box mt="24" p="2">
-      <c-input
-        type="text"
-        placeholder="search location"
-        variant="outline"
-        size="md"
-        v-model="query"
-        @keypress="fetchWeather"
-      />
-    </c-box>
-    <c-box align-item="center" v-if="weather && !notFound">
-      <c-text align="center">{{ location }}, {{ country }}</c-text>
-      <c-text align="center">{{ date }} </c-text>
-      <c-text align="center">{{ temp }} &deg;C</c-text>
-      <c-text align="center">{{ weather }}</c-text>
-    </c-box>
-    <c-box v-if="notFound">
-      <c-text align="center"> Location not found ! </c-text>
-    </c-box>
-  </c-stack>
+  <c-box :class="changeBackground()" h="100vh" p="2">
+    <c-stack maxW="sm" mx="auto">
+      <c-box mt="24">
+        <c-input
+          type="text"
+          placeholder="search location"
+          variant="outline"
+          size="md"
+          v-model="query"
+          @keypress="fetchWeather"
+        />
+      </c-box>
+      <c-box
+        class="weather-box"
+        mt="6"
+        p="2"
+        border="1px solid lightgrey"
+        rounded="lg"
+        shadow="md"
+        v-if="weather && !notFound"
+      >
+        <c-text align="center" fontSize="2xl" mb="2"
+          >{{ location }}, {{ country }}</c-text
+        >
+        <c-text align="center" mb="1">{{ date }} </c-text>
+        <c-text align="center" mb="1">{{ temp }} &deg;C</c-text>
+        <c-text align="center" mb="1">{{ weather }}</c-text>
+      </c-box>
+      <c-box v-if="notFound" mt="6">
+        <c-text align="center"> Location not found ! </c-text>
+      </c-box>
+    </c-stack>
+  </c-box>
 </template>
 
 <script lang="js">
@@ -70,7 +82,97 @@ export default {
       this.notFound = true;
       this.query = '';
     },
+    changeBackground() {
+      switch (this.weather) {
+        case "Clear":
+        return "weather-clear-sky"
+        break;
+        case "Clouds":
+        return "weather-cloudy"
+        break;
+        case "Rain":
+        return "weather-rain"
+        break;
+        case "Snow":
+        return "weather-snow"
+        break;
+        case "Atmosphere":
+        return "weather-atmosphere"
+        break;
+        default:
+        return "weather-not-found"
+        break;
+      }
+    }
   },
-
 }
 </script>
+
+<style scoped>
+.weather-clear-sky {
+  background: linear-gradient(
+    to bottom,
+    #ffffff,
+    #ecf1ff,
+    #cfe5ff,
+    #a5ddff,
+    #6dd5fa
+  );
+}
+.weather-cloudy {
+  background: linear-gradient(
+    to bottom,
+    #ffffff,
+    #faf9fa,
+    #f6f4f4,
+    #f2eeed,
+    #ece9e6
+  );
+}
+.weather-rain {
+  background: linear-gradient(
+    to bottom,
+    #485563,
+    #3d3e4a,
+    #2e2a31,
+    #1c181b,
+    #000000
+  );
+}
+.weather-snow {
+  background: linear-gradient(
+    to bottom,
+    #e6dada,
+    #baaeb5,
+    #898693,
+    #56626f,
+    #274046
+  );
+}
+.weather-snow {
+  background: linear-gradient(
+    to bottom,
+    #e6dada,
+    #baaeb5,
+    #898693,
+    #56626f,
+    #274046
+  );
+}
+.weather-atmosphere {
+  background: linear-gradient(
+    to bottom,
+    #e6dada,
+    #baaeb5,
+    #898693,
+    #56626f,
+    #274046
+  );
+}
+.weather-not-found {
+  background-color: #f8f8f8;
+}
+.weather-box {
+  backdrop-filter: blugrayscaler(30%);
+}
+</style>
